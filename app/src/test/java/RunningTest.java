e9a4885ec06376f2691f9b34f58a881703c43425
Meng Li <meng.li@signavio.com>
2022-01-05 09:49:57 +0100

    created unit test file

---
+++ app/src/test/java/RunningTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RunningTest {
+	@Test
+	public void test1(){
+		System.out.println("It worked !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		Assertions.assertEquals(1,1);
+	}
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "DependabotProjectWithGradleKotlinDSL"
-
+include("app")
