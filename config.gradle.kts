cbf11407769cf2f9ea16a4463bc0248d61f50f0f
Meng Li <meng.li@signavio.com>
2022-01-11 15:47:03 +0100

    Testing for method2

--- config.gradle.kts
+++
@@ -1,9 +0,0 @@
-mapOf(
-    Pair("minSdkVer", 22),
-    Pair("targetSdkVer", 25),
-    Pair("compiledSdkVer", 25),
-    Pair("buildToolsVer", "26-rc4"),
-    Pair("jupiterVersion","5.6.0")
-).entries.forEach {
-    project.extra.set(it.key, it.value)
-}
--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -47,4 +47,12 @@
 //    )
 //)
 
-val jupiterVersion by extra { "5.6.0" }
+mapOf(
+    Pair("minSdkVer", 22),
+    Pair("targetSdkVer", 25),
+    Pair("compiledSdkVer", 25),
+    Pair("buildToolsVer", "26-rc4"),
+    Pair("jupiterVersion","5.6.0")
+).entries.forEach {
+    project.extra.set(it.key, it.value)
+}
